bbaaedbbb240bb5930e7b3529cd453ec7de423a1
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-04-01 07:51:41 +0500

    Task 8 - Learn JavaScript Fundamentals Completed

---
+++ T7-JavaScript-Fundamentals/main.js
@@ -0,0 +1,82 @@
+// // Logging a statement using console.log
+// console.log("Hello World");
+
+// // Variables
+
+// let age = 30;
+// console.log(age);
+
+// const salary = 95959595;
+// console.log(salary);
+
+// const name = "Shehzad";
+// const age = 99;
+// const happy = true;
+// const hehe = null;
+// const hoho = undefined;
+
+// const person = {
+//   firstName: "Shehzad",
+//   lastName: "Aslam",
+//   age: 21,
+// };
+
+// console.log(person.firstName);
+
+// const nums = [1, 2, 4, 5, 6, 7, 3];
+// console.log(nums[4]);
+
+// let x = 3;
+// let y = 6;
+
+// console.log(x / y);
+// console.log(x * y);
+// console.log(x - y);
+// console.log(x++);
+// console.log(--x);
+
+// console.log(x < y && y > x);
+// console.log(x < y);
+// console.log(x > y || y > x);
+// console.log(!(x < y && y > x));
+
+// console.log("Muhammad Shehzad");
+
+// const isEven = 10 % 2 == 0 ? true : false;
+// console.log(isEven);
+
+// console.log(5 - "4");
+// console.log(5 + undefined);
+// console.log("5" + null);
+// console.log("5" + true);
+// console.log(true - 5);
+
+// console.log(Number("3.234"));
+// console.log(Boolean(-1));
+// console.log((500).toString());
+
+// let a = 10;
+// let c = 45;
+// let b = "10";
+
+// console.log(a == b);
+// console.log(a === b);
+
+// if (a < c) {
+//   console.log("yppppp");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Value = " + i);
+// }
+
+// const arr = [234, 234, 4, 23, 4, 45, 234, 53, 5, 234, 5, 3];
+// for (let num of arr) {
+//   console.log("heheheh" + num);
+// }
+
+const plus = (a, b) => {
+  console.log(a + b);
+};
+
+plus(2, 5);
